64bfad600dfa990ef5659107a5b750a5b85d24ce
bguiz <bguiz@users.noreply.github.com>
2019-06-12 15:56:54 +0800

    proj: init

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Brendan Graetz', // Required
+  author: 'bguiz', // Required
+  description: 'the blog that Brendan writes',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://bguiz-blog.netlify.com/',
+    github: 'https://github.com/bguiz',
+    twitter: 'https://twitter.com/bguiz',
+    linkedin: 'https://www.linkedin.com/in/brendangraetz/',
+  },
+  // pathPrefix: '',
+  siteUrl: 'https://bguiz-blog.netlify.com/',
+};
